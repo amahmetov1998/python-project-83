e2dbb39c0c909e7b9cfe9666cd428218e045d89f
Your Name <you@example.com>
2023-06-22 10:30:31 +0500

    git add .

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -18,19 +18,41 @@ def main():
         connect.autocommit = True
         with connect.cursor() as curs:
             curs.execute(
-                        'DROP TABLE IF EXISTS urls'
-                    )
-            curs.execute(
-                '''CREATE TABLE urls (
+                '''CREATE TABLE IF NOT EXISTS urls (
                 id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                 name VARCHAR(255),
-                created_at DATE NOT NULL);'''
-            )
+                created_at DATE NOT NULL);''')
+
+            curs.execute(
+                '''CREATE TABLE IF NOT EXISTS url_checks (
+                id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                url_id bigint REFERENCES urls (id),
+                status_code INTEGER,
+                h1 VARCHAR(255),
+                title VARCHAR(255),
+                description VARCHAR(255),
+                created_at DATE);''')
+
     except Exception as err:
         return err
     url = {'url': ''}
 
     return render_template('main.html', url=url)
+
+
+@app.route('/drop')
+def main_2():
+    try:
+        connect = psycopg2.connect(DATABASE_URL)
+        connect.autocommit = True
+        with connect.cursor() as curs:
+            curs.execute(
+                '''DROP TABLE IF EXISTS url_checks''')
+            curs.execute(
+                '''DROP TABLE IF EXISTS urls''')
+        return 'True'
+    except Exception as err:
+        return err
 
 
 @app.route('/urls', methods=['POST'])
@@ -58,8 +80,7 @@ def add_url():
         with connect.cursor() as curs:
             curs.execute('''
             SELECT * FROM urls;
-            '''
-                         )
+            ''')
             data = curs.fetchall()
 
     except Exception as err:
@@ -73,8 +94,10 @@ def add_url():
                     SELECT id FROM urls WHERE name=%s;
                     ''', (url,))
                     url_id = curs.fetchone()[0]
+
             except Exception as err:
                 return err
+
             flash('Страница уже существует', 'warning')
             return redirect(f'/urls/{url_id}')
 
@@ -82,18 +105,16 @@ def add_url():
         connect = psycopg2.connect(DATABASE_URL)
         connect.autocommit = True
         with connect.cursor() as curs:
+
             curs.execute('''
             INSERT INTO urls (name, created_at)
             VALUES (%s, %s);
-            ''', (url, date.today())
-                         )
+            ''', (url, date.today()))
 
-        with connect.cursor() as curs:
             curs.execute('''
             SELECT id FROM urls WHERE name=%s;
             ''', (url,))
             url_id = curs.fetchone()[0]
-            # return redirect(url_for('urls', id={curs.fetchone()[0]}))
 
     except Exception as err:
         return err
@@ -111,14 +132,19 @@ def get_url(url_id):
         connect = psycopg2.connect(DATABASE_URL)
         with connect.cursor() as curs:
             curs.execute('''
-            SELECT * FROM urls WHERE id=%s;
+            SELECT * FROM urls WHERE id=%s
             ''', (url_id,))
             data = curs.fetchall()
+
+            curs.execute('''
+            SELECT * FROM url_checks WHERE url_id=%s
+            ''', (url_id,))
+            info = curs.fetchall()
 
     except Exception as err:
         return err
 
-    return render_template('url.html', data=data, messages=messages)
+    return render_template('url.html', data=data, info=info, messages=messages)
 
 
 @app.get('/urls')
@@ -127,7 +153,12 @@ def get_urls():
         connect = psycopg2.connect(DATABASE_URL)
         with connect.cursor() as curs:
             curs.execute('''
-            SELECT * FROM urls ORDER BY id DESC;
+            SELECT url_checks.url_id, urls.name, MAX(url_checks.created_at)
+            FROM url_checks
+            JOIN urls
+            ON urls.id = url_checks.url_id
+            GROUP BY url_checks.url_id, urls.name
+            ORDER BY url_checks.url_id DESC
             ''')
             data = curs.fetchall()
 
@@ -135,3 +166,20 @@ def get_urls():
         return err
 
     return render_template('urls.html', data=data)
+
+
+@app.route('/urls/<int:url_id>/checks', methods=['POST'])
+def check_urls(url_id):
+    try:
+        connect = psycopg2.connect(DATABASE_URL)
+        connect.autocommit = True
+        with connect.cursor() as curs:
+            curs.execute('''
+            INSERT INTO url_checks (url_id, created_at)
+            VALUES (%s, %s)
+            ''', (url_id, date.today(),))
+
+    except Exception as err:
+        return err
+    flash('Страница успешно проверена', 'success')
+    return redirect(f'/urls/{url_id}')
